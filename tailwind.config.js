97e99e98b8869a7e811b932c11e9af0f75f0e3a4
mehdi <siavhnz@gmail.com>
2022-08-23 10:45:22 +0430

    mobile design added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: [],
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    screens: {
+      xs: "475px",
+      // => @media (min-width: 475px) { ... }
+      ...defaultTheme.screens,
+    },
+    extend: {
+      colors: {
+        cream: "#f2ebe3",
+        "dark-grayish-blue": "#6c7289",
+        "very-dark-blue": "#1c232b",
+        "dark-cyan": "#3c8067",
+      },
+      fontFamily: {
+        fraunces: ["Fraunces", "serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      letterSpacing: {
+        "extra-wide": ".4em",
+      },
+    },
   },
   plugins: [],
-}
+};
